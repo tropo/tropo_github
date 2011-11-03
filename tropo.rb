55ec6ec5591a4b868d55a5ae48df71346fb61843
John Dyer <john@krumpt.com>
2011-11-02 21:10:19 -0400

    defaulting to SMS, but voice can be provided

--- tropo.rb
+++ tropo.rb
@@ -1,8 +1,6 @@
-from_number = $from_number ? $from_number : ""
 network = $network ? $network : 'SMS'
 
 message $msg_to_send, {
     :to=>$destination,
-    :callerID=>from_number,
     :network => network
   }
