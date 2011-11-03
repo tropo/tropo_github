c78ace06e6c128577f160e1ed7ad74297bf5636f
John Dyer <john@krumpt.com>
2011-11-02 21:08:53 -0400

    testing

--- tropo.rb
+++ tropo.rb
@@ -1,5 +1,8 @@
+from_number = $from_number ? $from_number : ""
+network = $network ? $network : 'SMS'
+
 message $msg_to_send, {
     :to=>$destination,
-    :callerID=>$from_number
-    
+    :callerID=>from_number,
+    :network => network
   }
