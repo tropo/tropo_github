0c64c8e1c1cba5456c3d4c7b9131441db00b111a
John Dyer <john@krumpt.com>
2011-11-02 21:37:19 -0400

    testing mulitple recipients

--- tropo.rb
+++ tropo.rb
@@ -1,6 +1,6 @@
 network = $network ? $network : 'SMS'
 
-$destination.split('%2C').each do |to|
+$destination.split(',').each do |to|
   message $msg_to_send, {
     :to=>to,
     :network => network
